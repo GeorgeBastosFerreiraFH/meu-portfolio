fb297fef89ad584ddc0910106a5bfbf21932dd11
GeorgeBastosFerreiraFH <114554305+GeorgeBastosFerreiraFH@users.noreply.github.com>
2025-02-19 00:25:26 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { BrowserRouter as Roteador } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Inicio from "./paginas/Inicio";
+import SobreMim from "./paginas/SobreMim";
+import Certificados from "./paginas/Certificados";
+import Projetos from "./paginas/Projetos";
 import Layout from "./components/Layout";
 import "./App.css";
 
 function App() {
   return (
-    <Roteador>
-      <Layout />
-    </Roteador>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Inicio />} />
+          <Route path="/projetos" element={<Projetos />} />
+          <Route path="/certificados" element={<Certificados />} />
+          <Route path="/sobre" element={<SobreMim />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
